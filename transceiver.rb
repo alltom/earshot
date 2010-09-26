b0ac5d37829b52c69ca3013236f41d0214e19765
mason <mason@ubuntu.ubuntu-domain>
2010-09-25 23:20:05 -0400

    implemented continuous movement for transceivers

--- transceiver.rb
+++ transceiver.rb
@@ -1,15 +1,55 @@
+WIDTH, HEIGHT = 900, 600
+MIN_SPEED, MAX_SPEED = 5, 20
 
 class Transceiver
-  attr_accessor :loc
   attr_accessor :airspace
   attr_accessor :outgoing_broadcast
   attr_accessor :range_oval
   attr_accessor :progress_oval
   
   def initialize(loc, airspace)
-    @loc = loc
+    @old_loc = loc
+    @new_loc = nil    # the agent's new destination
+    @speed = nil      # the agent's speed
+    @move_start = nil # the time at which the agent started moving
     @airspace = airspace
     @stored_messages = []
+  end
+
+  def loc
+    if @new_loc.nil?
+      # self isn't moving
+      return @old_loc
+    end
+
+    time_to_dest = @old_loc.dist(@new_loc)/@speed
+    time_since_move = $shreduler.now - @move_start
+
+    if time_since_move > time_to_dest
+      # self already arrived
+      @old_loc = @new_loc
+      @new_loc = nil
+      @speed = nil
+      @move_start = nil
+      return @old_loc
+    end
+
+    # self is moving, so calculate its position
+    xv = (@new_loc.x - @old_loc.x)/time_to_dest
+    yv = (@new_loc.y - @old_loc.y)/time_to_dest
+    cur_x = @old_loc.x + xv*time_since_move
+    cur_y = @old_loc.y + yv*time_since_move
+    cur_loc = Loc.new(cur_x, cur_y)
+    return cur_loc
+  end
+
+  def move(new_loc, speed)
+    # just in case this agent was already moving, update @cur_loc
+    @cur_loc = loc 
+
+    @new_loc = new_loc
+    @speed = speed
+    @move_start = $shreduler.now
   end
   
   def broadcast_message(message)
@@ -31,6 +71,15 @@ class Transceiver
         broadcast = broadcast_message(@stored_messages[rand @stored_messages.length])
         Shred.yield(SECONDS_PER_BIT * broadcast.message.length)
       end
+    end
+
+    spork_loop do
+      Shred.yield(rand * 20)
+
+      new_loc = Loc.new((rand * WIDTH).to_i, (rand * HEIGHT).to_i) 
+      speed = rand*(MAX_SPEED-MIN_SPEED) + MIN_SPEED
+      move(new_loc, speed)
+      LOG.info "#{self} started moving to #{new_loc} with speed #{speed}"
     end
   end
   
