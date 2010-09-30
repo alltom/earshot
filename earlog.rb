d20dd1755fc288a556ecfae5fc9beb28e7a32772
Mason Simon <masonsimon@gmail.com>
2010-09-29 23:48:01 -0400

    forgot to add this module before

---
+++ earlog.rb
@@ -0,0 +1,21 @@
+class EarLog
+  def initialize(outfile)
+    @outfile = outfile
+  end
+
+  def xmit(relay, dest, message)
+    @outfile.puts "#{$shreduler.now}\t#{relay.uid}\txmit\t#{dest.uid}\t#{message.uid}\t#{message.length}"
+  end
+
+  def recv(agent, message)
+    @outfile.puts "#{$shreduler.now}\t#{agent.uid}\trecv\t#{message.uid}"
+  end
+
+  def move(agent, xo, yo, speed)
+    @outfile.puts "#{$shreduler.now}\t#{agent.uid}\tmove\t#{xo}\t#{yo}\t#{speed}"
+  end
+
+  def bump()
+    @outfile.puts "#{$shreduler.now}\tbump"
+  end
+end
