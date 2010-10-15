53c00fc7e11e35930e91bb28c95895e01f695bac
Mason Simon <masonsimon@gmail.com>
2010-10-15 00:20:02 -0400

    added a bunch of failing tests. tested that the initial state is idle

--- test/agent.rb
+++ test/agent.rb
@@ -15,12 +15,32 @@ class UIDGenerator
 end
 
 
-
-
 class TransmitterTester < Test::Unit::TestCase
   def test_init
     loc = nil
     airspace = nil
     t = Transmitter.new(loc, airspace)
+
+    assert_equal :idle, t.state
+  end
+
+  def test_broadcast_message
+    assert false
+  end
+
+  def test_receive_start_flag
+    assert false
+  end
+
+  def test_receive_length
+    assert false
+  end
+
+  def test_receive_checksum
+    assert false
+  end
+
+  def test_receive_message
+    assert false
   end
 end
