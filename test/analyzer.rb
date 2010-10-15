88001580d718e875953d5ba28c4454c2ce825a96
Mason Simon <masonsimon@gmail.com>
2010-10-14 20:48:03 -0400

    added analyzer testfile

---
+++ test/analyzer.rb
@@ -0,0 +1,29 @@
+require 'test/unit'
+require './analyzer'
+
+
+class Tester < Test::Unit::TestCase
+  def test_born
+    false
+  end
+
+  def test_xmit
+    false
+  end
+
+  def test_recv
+    false
+  end
+
+  def test_move
+    false
+  end
+
+  def test_bump
+    false
+  end
+
+  def test_relay
+    false
+  end
+end
