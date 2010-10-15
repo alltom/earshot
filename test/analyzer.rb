59c46b9c358fa3313520690efce0fbaf9d0aba1d
Mason Simon <masonsimon@gmail.com>
2010-10-14 20:53:59 -0400

    tested born

--- test/analyzer.rb
+++ test/analyzer.rb
@@ -2,28 +2,40 @@ require 'test/unit'
 require './analyzer'
 
 
+LOC_X, LOC_Y = 15, 27
+AGENT_UID = '1234'
+MESSAGE_UID = '9076'
+MESSAGE_LENGTH = 27
+NOW = 8736
+
+class Loc
+end
+
 class Tester < Test::Unit::TestCase
   def test_born
-    false
+    a = Analyzer.new
+    n = a.num_agents
+    a.puts("#{NOW}\tborn\t#{AGENT_UID}\t#{LOC_X}\t#{LOC_Y}")
+    assert_equal n+1, a.num_agents
   end
 
   def test_xmit
-    false
+    assert false
   end
 
   def test_recv
-    false
+    assert false
   end
 
   def test_move
-    false
+    assert false
   end
 
   def test_bump
-    false
+    assert false
   end
 
   def test_relay
-    false
+    assert false
   end
 end
