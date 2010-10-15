23ccc6ab1b05c1569aa2de663537303b7a59f702
Mason Simon <masonsimon@gmail.com>
2010-10-15 00:10:54 -0400

    got init test passing

--- test/agent.rb
+++ test/agent.rb
@@ -2,8 +2,25 @@ require 'test/unit'
 require './agent'
 
 
+# Mock classes
+UID = '123123'
+
+class UIDGenerator
+  def initialize(prefix)
+  end
+
+  def next
+    UID
+  end
+end
+
+
+
+
 class TransmitterTester < Test::Unit::TestCase
   def test_init
-    t = Transmitter.new
+    loc = nil
+    airspace = nil
+    t = Transmitter.new(loc, airspace)
   end
 end
