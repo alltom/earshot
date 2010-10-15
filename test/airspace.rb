b1abb1ccdfb494a4ae2a93c344474eec5005eba9
Mason Simon <masonsimon@gmail.com>
2010-10-14 21:19:59 -0400

    tested sending a bit with no one else around

--- test/airspace.rb
+++ test/airspace.rb
@@ -3,22 +3,42 @@ require './airspace'
 
 
 # Mock classes
+RADIUS = 50
+
 class Loc
   attr_reader :x, :y
   def initialize(x, y)
     @x, @y = x, y
   end
-end
 
-class Agent
-  attr_reader :loc
-  def initialize(loc)
-    @loc = loc
+  def dist(loc)
+    Math.sqrt((@x - loc.x) ** 2 + (@y - loc.y) ** 2)
   end
 end
 
+class Agent
+  attr_reader :loc, :bit
+  def initialize(loc)
+    @loc = loc
+    @bit = nil
+  end
+
+  def recv_bit(bit)
+    @bit = bit
+  end
+end
+
+
 class Tester < Test::Unit::TestCase
-  def test_send_bit
-    assert false
+  def test_send_bit_isolated
+    x, y = 1, 2
+    bit = '1'
+
+    air = Airspace.new
+    agent = Agent.new(Loc.new(x, y))
+    air << agent
+    air.send_bit(agent, RADIUS, bit)
+
+    assert agent.bit.nil?
   end
 end
