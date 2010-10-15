4f6e0f69f1d5e602213554981dabf07c579e7c70
Mason Simon <masonsimon@gmail.com>
2010-10-14 21:49:53 -0400

    added testfile for simulation

---
+++ test/simulation.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+require './simulation'
+
+
+class Tester < Test::Unit::TestCase
+  def test_add_agent
+    assert false
+  end
+
+  def test_advance
+    assert false
+  end
+end
