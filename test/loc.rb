164bc96ff8149e7c17e630231e3b8407750d416f
Mason Simon <masonsimon@gmail.com>
2010-10-14 20:07:10 -0400

    added testfile for Loc and Rakefile to run the test

---
+++ test/loc.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+require './loc'
+
+class Tester < Test::Unit::TestCase
+  def test_dist
+    l1 = Loc.new(0, 0)
+    l2 = Loc.new(0, 1)
+    assert_equal 1, l1.dist(l2)
+  end
+end
